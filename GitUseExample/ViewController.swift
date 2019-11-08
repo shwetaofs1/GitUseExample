26ff6d4ebfc3a58153f061d6540f7feea7ecf70d
Apple <apple@apples-mac-mini.local>
2019-11-08 11:19:36 +0530

    Added one more log

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
           print("This is a Git tutorial")
+         print("This is a Git tutorial1")
     }
 
     
